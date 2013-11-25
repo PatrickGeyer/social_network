158497b0568caa24c46051f63a01d142e7ae275d
PatrickGeyer <patrick.geyer1@gmail.com>
2013-11-25 21:13:24 +0100

    First Commit

---
+++ ErrorPages/404.php
@@ -0,0 +1,19 @@
+<?php
+	include_once($_SERVER['DOCUMENT_ROOT']."/Scripts/lock.php");
+	include_once($_SERVER['DOCUMENT_ROOT'].'/welcome.php');
+	include_once ($_SERVER['DOCUMENT_ROOT'].'/chat.php');
+?>
+<html>
+	<head>
+		<title>Page not Found</title>
+	</head>
+	<body>
+		<center>
+			<br><br><br>
+			<div style="background-color:lightgrey;border-radius:20px;padding:10px; border:1px solid grey; margin-left:40%; margin-right:40%;">
+				<h1>Error 404</h1>
+				<h2>Sorry, the resource you are looking for was not found.</h2>
+			</div>
+		</center>
+	</body>
+</html>
---
+++ ErrorPages/405.php
@@ -0,0 +1,4 @@
+<?php
+	include_once('../welcome.php');
+echo "Error 405";
+?>
