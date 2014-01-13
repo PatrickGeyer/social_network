28abca430a6b81cfbdbbe6cc2e539789aa65814b
PatrickGeyer <patrick.geyer1@gmail.com>
2014-01-13 23:04:47 +0100

    Added File paths for icons

--- Scripts/base.class.php
+++ Scripts/base.class.php
@@ -8,5 +8,14 @@ class Base {
     const FEMALE_DEFAULT_CHAT = "Images/male-default-icon.png";
     
     const IMAGE_QUALITY = 60;
+    
+    const VIDEO_THUMB = "Images/Icons/file_type/play-red.png";
+    const AUDIO_THUMB = "Images/Icons/file_type/music-blue.png";
+    const IMAGE_THUMB = "Images/Icons/Icon_Pacs/typicons.2.0/png-24px/image.png";
+    const FOLDER_THUMB = "Images/Icons/Icon_Pacs/ecqlipse2/system_black/FOLDER - OPEN_48x48-32.png";
+    const PDF_THUMB = "Images/Icons/file_type/pdficon_large.png";
+    const WORD_THUMB = "Images/Icons/file_type/Microsoft_Word_2013_logo.png";
+    const POWERPOINT_THUMB = "Images/Icons/file_type/Microsoft_PowerPoint_2013_logo.png";
+    const EXCEL_THUMB = "Images/Icons/file_type/Microsoft_Excel_2013_logo.png";
 }
 ?>
